dc035a58ed93e7826854beb0eb8545d44c423db9
Ken McCormack <kenmccormack@gmail.com>
2016-06-26 17:01:13 +1000

    Example C# strict Rhino Mocks, Moq and stubs

---
+++ cs/MockTests/MoqTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Moq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Shokunin.MockTests
+{
+    public interface IFakeTest
+    {
+        int A();
+
+        string B();
+
+        decimal C();
+    }
+
+    [TestClass]
+    public class MoqTest
+    {
+        [TestMethod]
+        public void TestAMoqMock()
+        {
+            var fake = new Mock<IFakeTest>();
+            fake.Setup(mock => mock.A()).Returns(1);
+
+            var sut = fake.Object;
+            
+            Assert.AreEqual(1, sut.A());
+
+            Console.WriteLine("B is :" + sut.B());
+
+            sut.C();
+
+            // Explicitly verify each expectation...
+            fake.Verify(mock => mock.A(), Times.Once());
+            
+            // Verify everything
+            fake.VerifyAll();
+        }
+
+
+        [TestMethod]
+        public void TestAMoqStub()
+        {
+            var fake = new Mock<IFakeTest>();
+            fake.Setup(mock => mock.A()).Returns(1);
+
+            var sut = fake.Object;
+
+            Assert.AreEqual(1, sut.A());
+
+            Console.WriteLine("B is :" + sut.B());
+
+            sut.C();
+
+            // Explicitly verify each expectation...
+            fake.Verify(mock => mock.A(), Times.Once());
+
+            // For stub behaviour, we just don't verify anything
+            // fake.VerifyAll();
+        }
+
+
+    }
+}
---
+++ cs/MockTests/RhinoMocksTest.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Rhino.Mocks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Shokunin.MockTests
+{
+    [TestClass]
+    public class RhinoMocksTest
+    {
+        [TestMethod]
+        public void TestARhinoMocksStrictMock()
+        {
+            // a strict mock is v fussy
+            var fake = MockRepository.GenerateStrictMock<IFakeTest>();
+
+            fake.Expect(mock => mock.A())
+                .Return(1)
+                .Repeat
+                .Once();
+
+            var sut = fake;
+            
+            Assert.AreEqual(1, sut.A());
+
+            // this will break sut.B();
+
+            fake.VerifyAllExpectations();
+        }
+
+        [TestMethod]
+        public void TestARhinoMocksStrictMock2Calls()
+        {
+            var fake = MockRepository.GenerateStrictMock<IFakeTest>();
+
+            fake.Expect(mock => mock.A())
+                .Return(1)
+                .Repeat
+                .Once();
+
+            fake.Expect(mock => mock.B())
+                .Return("hello")
+                .Repeat
+                .Once();
+
+            fake.Expect(mock => mock.C())
+                .Return(2.3m)
+                .Repeat
+                .Once();
+
+            var sut = fake;
+
+            Assert.AreEqual(1, sut.A());
+
+            sut.C();
+            sut.B();
+
+            fake.VerifyAllExpectations();
+        }
+
+        [TestMethod]
+        public void TestARhinoMocksStub()
+        {
+            var fake = MockRepository.GenerateStub<IFakeTest>();
+
+            fake.Expect(mock => mock.A())
+                .Return(1)
+                .Repeat
+                .Once();
+
+            fake.Expect(mock => mock.B())
+                .Return("hello")
+                .Repeat
+                .Once();
+
+            //fake.Expect(mock => mock.C())
+            //    .Return(2.3m)
+            //    .Repeat
+            //    .Once();
+
+            var sut = fake;
+
+            Assert.AreEqual(1, sut.A());
+
+            Assert.AreEqual(0m, sut.C());
+
+            sut.C();
+            //sut.B();
+
+            fake.VerifyAllExpectations();
+        }
+
+
+    }
+}
