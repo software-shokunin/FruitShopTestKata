3e761d28ed07ee08bd594bfd430c6573b67a296e
Ken McCormack <Ken.McCormack@myob.com>
2016-06-28 18:45:12 +1000

    Ignore production.spec.js

--- js/test/06 production.spec.js
+++ js/test/06 production.spec.js
@@ -1,7 +1,7 @@
 let expect = require('chai').expect;
 
 // ignored for now
-describe('06 ProductionSpec:', () => {
+xdescribe('06 ProductionSpec:', () => {
 
     let ReplayProductionTraffic = require('./05 productionFailExamples');
     let FruitShop = require('../src/fruitshopApi');
